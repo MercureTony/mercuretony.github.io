b8f9d63ed899155deede076b0c1f8b4ac18b051b
Anthony Uyende <anthony.m.uyende@gmail.com>
2024-10-05 13:36:03 -0400

    refactor: packages updated

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+})
 
-module.exports = nextConfig
+module.exports = withMDX({
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+})
