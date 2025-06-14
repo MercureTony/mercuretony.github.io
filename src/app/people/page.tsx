bbca46c4e680b45e9e4859b16ac29050422c8a68
Anthony Uyende <mercuretony@gmail.com>
2025-06-13 22:15:45 -0400

    Add Santosh Passoubady to people list

--- src/app/people/page.tsx
+++ src/app/people/page.tsx
@@ -14,6 +14,7 @@ const people: Person[] = [
   { name: "Paul Washer", url: "https://heartcrymissionary.com/about/mission-and-methodology/staff/paul-washers-bio/" },
   { name: "Shahvir Sarkary", url: "https://www.shahvirsarkary.com/" },
   { name: "Immad Akhund", url: "https://x.com/immad" },
+  { name: "Santosh Passoubady", url: "https://www.google.com/search?q=santosh+passoubady&client=ms-android-rogers-ca-revc&sca_esv=3035149b170ff626&sxsrf=AE3TifPLXVmb-e3GaN1RE00Z5gQJQsylXg%3A1749862790793&ei=hslMaOuYMM2pptQP9NKTyQc&oq=santosh+passoubady&gs_lp=EhNtb2JpbGUtZ3dzLXdpei1zZXJwIhJzYW50b3NoIHBhc3NvdWJhZHkyBBAjGCcyChAjGIAEGCcYigUyBRAhGKABMgUQABjvBUj5E1C7CVjYEXAEeACQAQKYAawBoAGcB6oBAzAuN7gBA8gBAPgBAZgCBqACmALCAggQABiwAxjvBcICCxAAGIAEGLADGKIEwgIFECEYnwWYAwCIBgGQBgSSBwM0LjKgB4sbsgcDMC4yuAeLAsIHBTAuMy4zyAcT&sclient=mobile-gws-wiz-serp" },
   // Add more people here...
 ].sort((a, b) => a.name.localeCompare(b.name)); // This sorts the list alphabetically
 
